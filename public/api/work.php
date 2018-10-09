1aefb873baba8f34aeec47c5458f67076e8ee665
TJ <toalcris@indiana.edu>
2018-10-09 17:09:56 -0400

    debug

--- public/api/work.php
+++ public/api/work.php
@@ -10,7 +10,7 @@ if ($taskId < 1) {
 */
 
 // 1. Go to the database and get all work associated with the $taskId
-$workArr = Work::getWorkByTaskId();
+$workArr = Work::getWorkByTaskId($task_id);
 
 // 2. Convert to JSON
 $json = json_encode($workArr, JSON_PRETTY_PRINT);
